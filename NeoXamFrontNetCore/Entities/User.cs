5e8d92e96ef45c9f070db1d1e1e288c68db50f61
Alaaeddine <AG-ha@LAPTOP-INODDO5O>
2019-07-12 00:18:41 +0100

    IGeneric Crud

---
+++ NeoXamFrontNetCore/Entities/User.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NeoXamFrontNetCore.Entities
+{
+
+    public class User
+    {
+
+        public long Id { get; set; }
+        public string Email { get; set; }
+        public string Nom { get; set; }
+        public string Photo { get; set; }
+        public string Prenom { get; set; }
+        public string Password { get; set; }
+        public DateTime RegisterDate { get; set; }
+        public string Role { get; set; }
+        public bool IsCompleted { get; set; }
+    }
+}
---
+++ NeoXamFrontNetCore/Infrasturcture/IGenericCrud.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NeoXamFrontNetCore.Infrasturcture
+{
+
+    public interface IGenericCrud<T> where T : class
+    {
+
+        void Add(T t);
+        void Update(long id, T t);
+        void Delete(long id);
+        T FindById(long id);
+        List<T> GetAll();
+
+    }
+}
